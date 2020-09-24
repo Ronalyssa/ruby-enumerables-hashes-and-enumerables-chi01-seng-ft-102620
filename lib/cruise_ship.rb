0547bc5539936626b972ad1bcd3fa802088bfa42
Ronalyssa Loiseau <rloiseau99@yahoo.com>
2020-09-24 21:16:31 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,8 +9,8 @@
 def select_winner(passengers)
   # add the code snippet here!
   winner = ""
-  passengers.each do |suite, name|
-    if suite == "suite_a" && name.start_with?("A")
+  array.each do |key, value|
+    if key == "suite_a" && value.start_with?("A")
       winner = value
     end
   end
